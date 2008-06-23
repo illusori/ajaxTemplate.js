d8eb07acd969c2e3c0d90abc7555c273db9dc45a
Sam Graham <git@illusori.co.uk>
2008-06-23 10:54:48 +0000

    Initial revision

---
+++ src/ajaxTemplate.js
@@ -0,0 +1,155 @@
+//  Support for AJAX requests modifying the document according to
+//  rules and templates defined within the document itself.
+//  Purpose is to allow the structure of the document to be defined
+//  in one place rather than building entirely in javascript via the DOM
+//  (a pain for non-developer designers) or having it partly in the document
+//  as XHTML and partly in the javascript via the DOM (a pain for everyone).
+//  Requires prototype.js 1.6.0.
+//  Copyright 2008 Sam Graham.
+
+var ajaxTemplate = {
+
+validActions: $w('replace append show hide'),
+
+applyAction: function ( fragmentNode, actionDef, valOb )
+{
+    var i, target, targets, prop, action, val;
+
+    for( i = 0; i < this.validActions.length; i++ )
+    {
+        if( prop = actionDef.getAttribute( 'ajt:' + this.validActions[ i ] ) )
+        {
+            action = this.validActions[ i ];
+            break;
+        }
+    }
+
+    if( !action )
+    {
+alert( 'unknown action' );
+        return;
+    }
+
+    val = valOb[ prop ];
+
+    if( !val && ( action == 'replace' || action == 'append' ) )
+        return;
+
+//alert( 'looking to ' + action + ' from ' + prop );
+
+    if( targets = actionDef.getAttribute( 'ajt:style' ) )
+    {
+//var ot = targets;
+        targets = fragmentNode.select( targets );
+//alert( 'style search for ' + ot + ' found ' + targets );
+    }
+    else if( targets = actionDef.getAttribute( 'ajt:xpath' ) )
+    {
+        alert( 'ajaxTemplate.js: xpath targets not yet supported' );
+        return;
+    }
+    else
+    {
+        //  Default target is span/div with same ajt:name attribute as prop
+        var attr = '[ajt:name=\'' + prop + '\']';
+        targets = fragmentNode.select( 'SPAN' + attr + ', DIV' + attr );
+//alert( 'default search for ' +
+//  'SPAN[ajt:name=\'' + prop + '\']' +
+//  ' found ' + target );
+    }
+
+//alert( action + ':' + prop + ' found targets ' + targets );
+    if( !targets )
+        return;
+
+    for( i = 0; i < targets.length; i++ )
+    {
+        target = $(targets[ i ]);
+
+//alert( 'target.nodeValue = ' + target.nodeValue ); 
+//alert( 'target.firstChild = ' + target.firstChild );
+
+        if( action == 'replace' )
+            if( target.nodeName == 'INPUT' )
+                target.value = val;
+            else
+                target.update( val );
+        else if( action == 'append' )
+            if( target.nodeName == 'INPUT' )
+                target.value += val;
+            else
+                target.insert( val );
+        else if( action == 'show' )
+            if( val )
+                target.show();
+            else
+                target.hide();
+        else if( action == 'hide' )
+            if( val )
+                target.hide();
+            else
+                target.show();
+    }
+},
+
+applyActions: function ( fragmentNode, actionsNode, valOb )
+{
+    $A($(actionsNode).select( 'LI' )).each(
+        function ( el )
+        {
+            ajaxTemplate.applyAction( fragmentNode, el, valOb );
+        } );
+},
+
+copyTemplate: function ( containerID, template, valOb )
+{
+    var templateNode, fragmentNode, actionsNode;
+
+//alert( 'looking for: #' + containerID + ' [ajt:template=\'' + template + '\']' );
+    templateNode = $(containerID).select(
+        '[ajt:template=\'' + template + '\']' ).first();
+    actionsNode = $(containerID).select(
+        '[ajt:actions=\'' + template + '\']' ).first();
+
+    if( !templateNode || !actionsNode )
+        return;
+
+//alert( 'templateNode ' + templateNode ); alert( templateNode.innerHTML );
+//alert( 'actionsNode ' + actionsNode ); alert( actionsNode.innerHTML );
+
+    fragmentNode = templateNode.cloneNode( true );
+
+//alert( "ajt:template is " + fragmentNode.getAttribute( 'ajt:template' ) );
+//  This crashes IE7 on the second time through?  Egads...
+//    $(fragmentNode).removeAttribute( 'ajt:template' );
+//alert( "attr removed" );
+//alert( "ajt:template now is " + fragmentNode.getAttribute( 'ajt:template' ) );
+    $(fragmentNode).show();
+
+    this.applyActions( fragmentNode, actionsNode, valOb );
+
+    return( fragmentNode );
+}
+
+};
+
+function ajtTest( containerID )
+{
+    var valOb = {
+        eveuser_id: 999,
+        register_date: 'register date goes here',
+        first_fetched: 'first_fetched date goes here',
+        limited_apikey: 'aaaabbbbccccddddeeeeffff'
+        };
+
+    var newChild =
+        ajaxTemplate.copyTemplate( containerID, 'eveuser_account', valOb );
+
+    if( !newChild )
+    {
+        alert( 'copyTemplate failed' );
+        return;
+    }
+
+    $(containerID).select( '.accountlist' )[ 0 ].appendChild( newChild );
+}

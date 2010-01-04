997278b00204e7fed522397402cff894b6a8789c
Sam Graham <git@illusori.co.uk>
2010-01-04 13:48:13 +0000

    Removed debug ajtTest() function. Indented properly. Updated copyright year.

--- src/ajaxTemplate.js
+++ src/ajaxTemplate.js
@@ -5,151 +5,140 @@
 //  (a pain for non-developer designers) or having it partly in the document
 //  as XHTML and partly in the javascript via the DOM (a pain for everyone).
 //  Requires prototype.js 1.6.0.
-//  Copyright 2008 Sam Graham.
+//  Copyright 2008-2010 Sam Graham.
 
 var ajaxTemplate = {
 
-validActions: $w('replace append show hide'),
+    Version: '1.0.0.1',
 
-applyAction: function ( fragmentNode, actionDef, valOb )
-{
-    var i, target, targets, prop, action, val;
+    validActions: $w('replace append show hide'),
 
-    for( i = 0; i < this.validActions.length; i++ )
-    {
-        if( prop = actionDef.getAttribute( 'ajt:' + this.validActions[ i ] ) )
+    applyAction: function ( fragmentNode, actionDef, valOb )
         {
-            action = this.validActions[ i ];
-            break;
-        }
-    }
+            var i, target, targets, prop, action, val;
 
-    if( !action )
-    {
+            for( i = 0; i < this.validActions.length; i++ )
+            {
+                if( prop = actionDef.getAttribute(
+                    'ajt:' + this.validActions[ i ] ) )
+                {
+                    action = this.validActions[ i ];
+                    break;
+                }
+            }
+
+            if( !action )
+            {
 alert( 'unknown action' );
-        return;
-    }
+                return;
+            }
 
-    val = valOb[ prop ];
+            val = valOb[ prop ];
 
-    if( !val && ( action == 'replace' || action == 'append' ) )
-        return;
+            if( !val && ( action == 'replace' || action == 'append' ) )
+                return;
 
 //alert( 'looking to ' + action + ' from ' + prop );
 
-    if( targets = actionDef.getAttribute( 'ajt:style' ) )
-    {
+            if( targets = actionDef.getAttribute( 'ajt:style' ) )
+            {
 //var ot = targets;
-        targets = fragmentNode.select( targets );
+                targets = fragmentNode.select( targets );
 //alert( 'style search for ' + ot + ' found ' + targets );
-    }
-    else if( targets = actionDef.getAttribute( 'ajt:xpath' ) )
-    {
-        alert( 'ajaxTemplate.js: xpath targets not yet supported' );
-        return;
-    }
-    else
-    {
-        //  Default target is span/div with same ajt:name attribute as prop
-        var attr = '[ajt:name=\'' + prop + '\']';
-        targets = fragmentNode.select( 'SPAN' + attr + ', DIV' + attr );
+            }
+            else if( targets = actionDef.getAttribute( 'ajt:xpath' ) )
+            {
+                alert( 'ajaxTemplate.js: xpath targets not yet supported' );
+                return;
+            }
+            else
+            {
+                //  Default target is span/div with same ajt:name attribute
+                //  as prop
+                var attr = '[ajt:name=\'' + prop + '\']';
+                targets = fragmentNode.select(
+                    'SPAN' + attr + ', DIV' + attr );
 //alert( 'default search for ' +
 //  'SPAN[ajt:name=\'' + prop + '\']' +
 //  ' found ' + target );
-    }
+            }
 
 //alert( action + ':' + prop + ' found targets ' + targets );
-    if( !targets )
-        return;
+            if( !targets )
+                return;
 
-    for( i = 0; i < targets.length; i++ )
-    {
-        target = $(targets[ i ]);
+            for( i = 0; i < targets.length; i++ )
+            {
+                target = $(targets[ i ]);
 
 //alert( 'target.nodeValue = ' + target.nodeValue ); 
 //alert( 'target.firstChild = ' + target.firstChild );
 
-        if( action == 'replace' )
-            if( target.nodeName == 'INPUT' )
-                target.value = val;
-            else
-                target.update( val );
-        else if( action == 'append' )
-            if( target.nodeName == 'INPUT' )
-                target.value += val;
-            else
-                target.insert( val );
-        else if( action == 'show' )
-            if( val )
-                target.show();
-            else
-                target.hide();
-        else if( action == 'hide' )
-            if( val )
-                target.hide();
-            else
-                target.show();
-    }
-},
+                if( action == 'replace' )
+                    if( target.nodeName == 'INPUT' )
+                        target.value = val;
+                    else
+                        target.update( val );
+                else if( action == 'append' )
+                    if( target.nodeName == 'INPUT' )
+                        target.value += val;
+                    else
+                        target.insert( val );
+                else if( action == 'show' )
+                    if( val )
+                        target.show();
+                    else
+                        target.hide();
+                else if( action == 'hide' )
+                    if( val )
+                        target.hide();
+                    else
+                        target.show();
+            }
+        },
 
-applyActions: function ( fragmentNode, actionsNode, valOb )
-{
-    $A($(actionsNode).select( 'LI' )).each(
-        function ( el )
+    applyActions: function ( fragmentNode, actionsNode, valOb )
         {
-            ajaxTemplate.applyAction( fragmentNode, el, valOb );
-        } );
-},
+            $A($(actionsNode).select( 'LI' )).each(
+                function ( el )
+                {
+                    ajaxTemplate.applyAction( fragmentNode, el, valOb );
+                } );
+        },
 
-copyTemplate: function ( containerID, template, valOb )
-{
-    var templateNode, fragmentNode, actionsNode;
+    copyTemplate: function ( containerID, template, valOb )
+        {
+            var templateNode, fragmentNode, actionsNode;
 
 //alert( 'looking for: #' + containerID + ' [ajt:template=\'' + template + '\']' );
-    templateNode = $(containerID).select(
-        '[ajt:template=\'' + template + '\']' ).first();
-    actionsNode = $(containerID).select(
-        '[ajt:actions=\'' + template + '\']' ).first();
+            templateNode = $(containerID).select(
+                '[ajt:template=\'' + template + '\']' ).first();
+            actionsNode = $(containerID).select(
+                '[ajt:actions=\'' + template + '\']' ).first();
 
-    if( !templateNode || !actionsNode )
-        return;
+//if( templateNode )
+//  alert( 'found templateNode' );
+//if( actionsNode )
+//  alert( 'found actionsNode' );
+
+            if( !templateNode || !actionsNode )
+                return;
 
 //alert( 'templateNode ' + templateNode ); alert( templateNode.innerHTML );
 //alert( 'actionsNode ' + actionsNode ); alert( actionsNode.innerHTML );
 
-    fragmentNode = templateNode.cloneNode( true );
+            fragmentNode = templateNode.cloneNode( true );
 
 //alert( "ajt:template is " + fragmentNode.getAttribute( 'ajt:template' ) );
 //  This crashes IE7 on the second time through?  Egads...
 //    $(fragmentNode).removeAttribute( 'ajt:template' );
 //alert( "attr removed" );
 //alert( "ajt:template now is " + fragmentNode.getAttribute( 'ajt:template' ) );
-    $(fragmentNode).show();
+            $(fragmentNode).show();
 
-    this.applyActions( fragmentNode, actionsNode, valOb );
+            this.applyActions( fragmentNode, actionsNode, valOb );
 
-    return( fragmentNode );
-}
+            return( fragmentNode );
+        }
 
-};
-
-function ajtTest( containerID )
-{
-    var valOb = {
-        eveuser_id: 999,
-        register_date: 'register date goes here',
-        first_fetched: 'first_fetched date goes here',
-        limited_apikey: 'aaaabbbbccccddddeeeeffff'
-        };
-
-    var newChild =
-        ajaxTemplate.copyTemplate( containerID, 'eveuser_account', valOb );
-
-    if( !newChild )
-    {
-        alert( 'copyTemplate failed' );
-        return;
-    }
-
-    $(containerID).select( '.accountlist' )[ 0 ].appendChild( newChild );
-}
+    };
